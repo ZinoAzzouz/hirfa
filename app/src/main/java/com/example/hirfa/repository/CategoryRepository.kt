3caad8bb39e2936fffb6c3e4c844617d2a2b0db3
Adberrahim <ghenaimabdelrrahim.39.dz@gmail.com>
2025-02-25 14:20:54 +0100

    TP2 done

--- app/src/main/java/com/example/hirfa/repository/CategoryRepository.kt
+++ app/src/main/java/com/example/hirfa/repository/CategoryRepository.kt
@@ -9,21 +9,34 @@ object CategoryRepository {
             id = "1",
             name = "Plumbing",
             description = "Plumbing services for homes and businesses",
-            icon = R.drawable.ic_plumbing
+            icon = R.drawable.plumber
         ),
         Category(
             id = "2",
             name = "Electrical",
             description = "Electrical repair and installation services",
-            icon = R.drawable.ic_electrical
+            icon = R.drawable.electrical
         ),
         Category(
             id = "3",
             name = "Cleaning",
             description = "Professional cleaning services",
-            icon = R.drawable.ic_cleaning
+            icon = R.drawable.cleaning
+        ),
+        Category(
+            id = "4",
+            name = "Carpenter",
+            description = "Professional cleaning services",
+            icon = R.drawable.carpenter
+        ),
+        Category(
+            id = "5",
+            name = "Chef",
+            description = "Professional cleaning services",
+            icon = R.drawable.chef
         )
     )
+
 
     fun getAllCategories(): List<Category> = categories
 }
